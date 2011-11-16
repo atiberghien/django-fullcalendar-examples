a52dc1e9b06ad5d972902a110a861a8a3c683674
Alban Tiberghien <alban.tiberghien@gmail.com>
2011-11-16 01:21:44 +0100

    Remove schedule from eventools branch

--- django_schedule_fullcalendar/myagenda/templatetags/strings.py
+++
@@ -1,9 +0,0 @@
-import re
-
-from django import template
-register = template.Library()
-
-@register.filter
-def replace (string, args):
-    find, replace = args.split(',')
-    return string.replace(find, replace)
